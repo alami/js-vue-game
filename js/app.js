c3a103a3832e900e6e7ede93650c91f6a26c7d35
alami <alami@km.ru>
2021-08-20 18:55:47 +0500

    ch12: add sound : fin all

--- js/app.js
+++ js/app.js
@@ -155,3 +155,25 @@ let app = new Vue({
         }
     }
 });
+let audio = new Audio('audio/soundtrack.mp3')
+let audio_btn = document.querySelector('.btn__sound')
+let audio_icon = document.querySelector('.btn__sound i')
+
+audio.muted = true
+audio.autoplay = true
+audio.volume = 0.2
+audio.addEventListener('loadedmetadata', //событие что звук был загружен
+    function () {
+        audio.currentTime = 0+Math.random() * audio.duration +1 - 0 //случайное начало всегда
+    })
+audio_btn.addEventListener('click',function (){
+    if (audio.muted) {
+        audio.muted = false
+        audio_icon.classList.add('fa-volume-up')
+        audio_icon.classList.remove('fa-volume-off')
+    } else {
+        audio.muted = true
+        audio_icon.classList.add('fa-volume-off')
+        audio_icon.classList.remove('fa-volume-up')
+    }
+})
