2e6618e2afae9e1694cf8de87bad3b6d6b4cb498
Aminov <alami@tshtt.uz>
2021-08-21 22:47:42 +0500

    ch02-06: ONLY 'v-bind' for 'attribut' value

---
+++ vue3/app0205.js
@@ -0,0 +1,9 @@
+const App = {
+    data() {
+        return {
+            placeholderString: "Введите название заметки",
+            title: "Список заметок"
+        }
+    }
+}
+Vue.createApp(App).mount('#app')
