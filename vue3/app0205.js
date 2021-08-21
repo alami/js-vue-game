82592ea353af0f7d6a8b4ee897c8f6781d683152
Aminov <alami@tshtt.uz>
2021-08-21 23:31:55 +0500

    ch02-07: both-dir binding v-on:...

--- vue3/app0205.js
+++ vue3/app0205.js
@@ -2,7 +2,14 @@ const App = {
     data() {
         return {
             placeholderString: "Введите название заметки",
-            title: "Список заметок"
+            title: "Список заметок",
+            inputValue: ''
+        }
+    },
+    methods: {
+        inputChangeHandler(event) {
+            // console.log("123 : ",event.target.value)
+            this.inputValue = event.target.value
         }
     }
 }
