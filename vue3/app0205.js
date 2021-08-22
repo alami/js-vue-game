7d85b92ad8405c0913139a092fea642e00e40dfe
alami <alami@km.ru>
2021-08-22 16:19:38 +0500

    ch02-11 remove note via this.notes.splice()

--- vue3/app0205.js
+++ vue3/app0205.js
@@ -14,6 +14,9 @@ const App = {
         addNewNote() {
             this.notes.push(this.inputValue)
             this.inputValue = ''
+        },
+        removeNote (idx) {
+            this.notes.splice(idx,1)
         }
     }
 }
