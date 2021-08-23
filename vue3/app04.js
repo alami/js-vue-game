ddf7d3d486f1f3e9ba6d15e4c523b571d0d3df64
alami <alami@km.ru>
2021-08-23 19:29:11 +0500

    ch04-02  render(){} - create Virtual DOM

--- vue3/app04.js
+++ vue3/app04.js
@@ -1,14 +1,21 @@
+const h = Vue.h
 const app= Vue.createApp({
     data() {
         return {
             title: 'test'
         }
     },
-    template: `
-       <div class="card center">
-         <h1>{{title}}</h1>
-         <button class="btn" @click="title='New title'"></button>
-       </div>
-     `
+    methods: {
+        changeTitle() {
+            this.title = 'New Title'
+        }
+    },
+    render() {
+        return h('div', {
+            class: 'card center',
+        }, [h('h1',{},this.title),
+            h('button',{class: 'btn', onClick: this.changeTitle }, 'Изменить')
+        ])
+    }
 })
 app.mount('#app')
