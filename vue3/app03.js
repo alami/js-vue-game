bb7ececef78913bd6839ae2f118bf9c890cd7a7c
alami <alami@km.ru>
2021-08-23 13:04:25 +0500

    ch03-05 v-for v1)inline 2)person 3)array -if -else -show

--- vue3/app03.js
+++ vue3/app03.js
@@ -1,9 +1,7 @@
 Vue.createApp({
-    /*data() {
-        return {
-            title: "Счетчик"
-        }
-    }*/
     // data: () => { return { title: "Счетчик" }}
-    data: () => ({ title: "Счетчик", myHTML: '<h1>Test Title</h1>' })
+    data: () => ({ title: "Счетчик", myHTML: '<h1>Test Title</h1>',
+        person: {firstName: 'Vlad', lastName: 'Minin', age: 27},
+        items: [1,2,3,4,5,6,7]
+    })
 }).mount('#app')
