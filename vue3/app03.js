78236c9763c72b5b3de451fac4efedb4a6b1f627
alami <alami@km.ru>
2021-08-23 16:52:28 +0500

    ch03-07 v-for .stop .prevente modificators & :key

--- vue3/app03.js
+++ vue3/app03.js
@@ -3,5 +3,10 @@ Vue.createApp({
     data: () => ({ title: "Счетчик", myHTML: '<h1>Test Title</h1>',
         person: {firstName: 'Vlad', lastName: 'Minin', age: 27},
         items: [1,2,3,4,5,6,7]
-    })
+    }),
+    methods: {
+        stopPropagation(event){
+            event.stopPropagation()
+        }
+    }
 }).mount('#app')
