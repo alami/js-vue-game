7c537fde5c5bc380b250e7beaa20ea1214931732
alami <alami@km.ru>
2021-08-24 19:29:27 +0500

    ch06-05: v4.v-for & v-bind:title=item

--- vue3/ch06/src/App.vue
+++ vue3/ch06/src/App.vue
@@ -2,9 +2,10 @@
   <div class="container pt-1">
     <div class="card">
       <h2>Актуальные новости {{now}}</h2>
-      <app-news title="This is title N-1"></app-news>
-      <app-news title="This is title N-2"></app-news>
-      <app-news title="This is title N-3"></app-news>
+      <app-news
+          v-for="item in news" :key="item"
+          :title="item"
+      ></app-news>
     </div>
   </div>
 </template>
