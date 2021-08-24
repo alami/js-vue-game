40fa45202e17a99a61e4721d937073003df3d8b7
alami <alami@km.ru>
2021-08-24 18:02:43 +0500

    ch06-02: 1st component TheHeader

--- vue3/ch06/src/App.vue
+++ vue3/ch06/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-
+    <h1>This is Title</h1>
+    <the-header></the-header>
   </div>
 </template>
 
---
+++ vue3/ch06/src/TheHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <h2>{{title}}</h2>
+  <hr />
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, harum.</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'The Header from Data'
+    }
+  }
+}
+</script>
+
