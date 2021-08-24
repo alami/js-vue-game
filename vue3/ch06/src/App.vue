5e78463a4bfb2762f5fea03e4ed472a65475f196
alami <alami@km.ru>
2021-08-24 18:30:37 +0500

    ch06-03: v1.news list - err:all news open

--- vue3/ch06/src/App.vue
+++ vue3/ch06/src/App.vue
@@ -1,14 +1,28 @@
 <template>
-  <div>
-    <h1>This is Title</h1>
-    <the-header></the-header>
+  <div class="container pt-1">
+    <div class="card">
+      <h2>Актуальные новости {{now}}</h2>
+    </div>
+    <div class="card" v-for="item in news" :key="item">
+      <h3>{{item}}</h3>
+      <button class="btn" @click="isOpen=!isOpen">Open</button>
+      <p v-if="isOpen">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa, odit.</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-  components: {
+  data  (){
+    return {
+      now: new Date().toLocaleDateString(),
+      isOpen: false,
+      news: [
+          'Джо победил на выборах',
+          'Вью 3 работате успешно',
+
+      ],
+    }
   }
 }
 </script>
