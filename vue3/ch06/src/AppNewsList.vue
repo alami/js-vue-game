07c790d3368e9593b3d42c5c75eafd46c6203e29
alami <alami@km.ru>
2021-08-25 19:25:46 +0500

    ch06-11: App->AppNewsList reactive

--- vue3/ch06/src/AppNewsList.vue
+++ vue3/ch06/src/AppNewsList.vue
@@ -2,7 +2,7 @@
   <hr />
   <h4>{{title}}</h4>
   <ul>
-    <li v-for="item in news">{{item.title}}</li>
+    <li v-for="item in news">{{item.title}} <span v-if="item.wasRead">!!!</span></li>
   </ul>
 </template>
 
