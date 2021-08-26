0ae72dfd76bcd32b40031b57874339e40ad78649
alami <alami@km.ru>
2021-08-26 10:59:59 +0500

    ch06-13: new empty App

---
+++ vue3/ch13/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container pt-1">
+    <div class="card">
+      <h2>Slots</h2>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
