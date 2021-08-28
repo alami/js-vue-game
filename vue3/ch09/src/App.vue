259644e61a37c1655b7cc57116c731c41f2ca36f
alami <alami@km.ru>
2021-08-28 17:48:34 +0500

    ch09: AppAlert component

--- vue3/ch09/src/App.vue
+++ vue3/ch09/src/App.vue
@@ -1,14 +1,28 @@
 <template>
   <div class="container">
+    <app-alert
+        v-if="alert"
+        title="Внимание!"
+        text="Это важное сообщение, будте бдительны"
+        type="danger"
+        closable
+        @close="alert=false"
+    ></app-alert>
     <div class="card">
-
+      <button class="btn primary" @click="alert=true">показать сообщение</button>
     </div>
   </div>
 </template>
 
 <script>
+import AppAlert from './components/AppAlert'
 export default {
-
+  data(){
+    return {
+      alert: false
+    }
+  },
+  components: {AppAlert},
 }
 </script>
 
