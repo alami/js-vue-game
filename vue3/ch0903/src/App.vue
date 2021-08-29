6223658fc9b7f5f33fbbdd167e538a1cb7ebe850
alami <alami@km.ru>
2021-08-29 18:08:33 +0500

    ch09-06: .blink modificator

--- vue3/ch0903/src/App.vue
+++ vue3/ch0903/src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="container">
     <div class="card">
-      <h2 v-color:[type]="mycolor">Ди⁠рективы</h2>
+      <h2 v-color:[type].blink="mycolor">Ди⁠рективы</h2>
       <div class="form-control">
         <label for="inp">Активный по умолчанию</label><br>
         <input type="text" id="inp" v-focus>
       </div>
-      <button class="btn" @click="mycolor='darkblue'">Сделать синим</button>
-      <button class="btn" @click="type=(type==='color'?'background-color':'color')">Переключить тип</button>
+      <button class="btn" @click="mycolor=mycolor==='darkblue'?'darkred':'darkblue'">Сделать синим</button>
+      <button class="btn" @click="type=type==='color'?'background-color':'color'">Переключить тип</button>
       {{type}}
     </div>
   </div>
--- vue3/ch0903/src/colorDirective.js
+++ vue3/ch0903/src/colorDirective.js
@@ -1,6 +1,13 @@
 export default {
     mounted(el, binding) {
         el.style[binding.arg] = binding.value
+        if (binding.modifiers.blink) {
+            let flag = true
+            setInterval(()=>{
+                el.style.color = flag?'#fff':binding.value
+                flag = !flag
+            }, 1000)
+        }
     },
     updated(el, binding) {
         el.style[binding.arg] = binding.value
