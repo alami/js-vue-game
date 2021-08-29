0cfa7b9b7672d54bdbb0056f4e300e19b68b071c
alami <alami@km.ru>
2021-08-29 19:32:48 +0500

    ch09-07: plug-in .use install {app.config.globalProperties$alert

--- vue3/ch0907/src/App.vue
+++ vue3/ch0907/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="container">
     <div class="card">
-
+    <h3>Plug-in</h3>
+      <button class="btn" @click="$alert('Wow It is working!')">Alert</button>
     </div>
   </div>
 </template>
--- vue3/ch0907/src/main.js
+++ vue3/ch0907/src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './theme.css'
+import translatePlugin from "../translatePlugin";
 
-createApp(App).mount('#app')
+const app=createApp(App)
+app.use(translatePlugin, {test:1})
+app.mount('#app')
