61222b0a0efeb429cb0408e26291bf900452effb
alami <alami@km.ru>
2021-08-29 17:11:38 +0500

    ch09-05: colorDirective dynamic [type]

--- vue3/ch0903/src/App.vue
+++ vue3/ch0903/src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="container">
     <div class="card">
-      <h2 v-color:background-color="mycolor">Ди⁠рективы</h2>
+      <h2 v-color:[type]="mycolor">Ди⁠рективы</h2>
       <div class="form-control">
         <label for="inp">Активный по умолчанию</label><br>
         <input type="text" id="inp" v-focus>
       </div>
       <button class="btn" @click="mycolor='darkblue'">Сделать синим</button>
+      <button class="btn" @click="type=(type==='color'?'background-color':'color')">Переключить тип</button>
+      {{type}}
     </div>
   </div>
 </template>
@@ -19,6 +21,7 @@ export default {
   data(){
     return {
       mycolor: 'darkred',
+      type: 'color',
     }
   },
   directives: {
