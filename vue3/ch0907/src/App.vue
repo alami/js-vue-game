62f6f11d7a8131934252ef2505916fbf17e37405
alami <alami@km.ru>
2021-08-30 13:02:13 +0500

    ch09-09: Modal Window, Teleport

--- vue3/ch0907/src/App.vue
+++ vue3/ch0907/src/App.vue
@@ -3,19 +3,30 @@
     <div class="card">
     <h3>{{$i18n('app.title')}}</h3>
       <button class="btn" @click="changeLang">{{$i18n('app.changeBtn')}}</button>
+      <button class="btn primary" @click="modal=true">Open modal window</button>
+      <teleport to="body">
+        <app-modal v-if="modal" @close="modal=false"></app-modal>
+      </teleport>
     </div>
   </div>
 </template>
 
 <script>
+import AppModal from './components/AppModal'
 export default {
+  data(){
+    return {
+      modal: false,
+    }
+  },
   inject:['changeI18N'],
   methods: {
     changeLang() {
       this.changeI18N('en')
       this.$forceUpdate()
     }
-  }
+  },
+  components: {AppModal},
 }
 </script>
 
---
+++ vue3/ch0907/src/components/AppModal.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="modal-backdrop" @click="$emit('close')"></div>
+<div class="modal">
+  <h3>This modal title</h3>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, tempora!</p>
+</div>
+</template>
+
+<script>
+export default {
+  emits: ['close'],
+}
+</script>
+
+<style scoped>
+
+</style>
