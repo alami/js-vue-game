b473f884341cc592d259fa82167299f2c94c9ade
alami <alami@km.ru>
2021-08-30 18:23:03 +0500

    ch10-04 <router-link to= WRAP button>

--- vue3/ch10/src/views/Forget.vue
+++ vue3/ch10/src/views/Forget.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="card">
     <h2>Забыл пароль? Так вспоминай!</h2>
-
-    <button class="btn" >Обратно к логину</button>
+    <router-link to="/login">
+      <button class="btn">Обратно к логину</button>
+    </router-link>
+    <button class="btn" @click="$router.push('/login')">Обратно к логину 2</button>
   </div>
 </template>
+<script>
+export default {
+  methods: {
+    goToLogin() {
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
