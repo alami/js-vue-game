ecfa0b09466b7aca9c02fa13ebe4808b55db1f6e
alami <alami@km.ru>
2021-09-01 18:40:31 +0500

    ch10-11 404-page

---
+++ vue3/ch10/src/views/NotFound.vue
@@ -0,0 +1,7 @@
+<template>
+<div class="card">
+  <h1>Стр-ца не найдена</h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, sed!</p>
+  <router-link to="/login">Логин</router-link>
+</div>
+</template>
