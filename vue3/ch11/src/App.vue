931afa4fc54a8248e104243062236093b4532541
alami <alami@km.ru>
2021-09-02 12:44:58 +0500

    ch11 counter-old local state

--- vue3/ch11/src/App.vue
+++ vue3/ch11/src/App.vue
@@ -1,8 +1,10 @@
 <template>
+  <the-navbar></the-navbar>
   <div class="container with-nav">
     <div class="card">
       <h1>Про Vuex</h1>
-      <h2>Счетчик 0</h2>
+      <h2>Счетчик {{counter}}</h2>
+      <button class="btn" @click="increment">++</button>
     </div>
   </div>
 </template>
@@ -10,6 +12,16 @@
 <script>
 import TheNavbar from './TheNavbar'
 export default {
+  data() {
+    return {
+      counter: 0,
+    }
+  },
+  methods: {
+    increment(){
+      this.counter++
+    }
+  },
   components: {TheNavbar}
 }
 </script>
