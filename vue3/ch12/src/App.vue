9ef8aa99a68c982d8d9ae4ec645fb69cfab38d39
alami <alami@km.ru>
2021-09-03 20:33:30 +0500

    ch12-17 newAPI with vuex

--- vue3/ch12/src/App.vue
+++ vue3/ch12/src/App.vue
@@ -4,6 +4,7 @@
       <ul class="navbar-menu">
         <li><router-link to="/">Home</router-link></li>
         <li><router-link to="/reusable">Переиспользование</router-link></li>
+        <li><router-link to="/vuex">Vuex</router-link></li>
       </ul>
   </header>
   <div class="container with-nav ">
