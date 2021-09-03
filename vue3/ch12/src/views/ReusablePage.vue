d8ab7c057bbd6ee30c36d008c60dc95b3c72c3d4
alami <alami@km.ru>
2021-09-03 17:47:08 +0500

    ch12-14 newAPI: router

--- vue3/ch12/src/views/ReusablePage.vue
+++ vue3/ch12/src/views/ReusablePage.vue
@@ -2,12 +2,23 @@
 <div class="card">
   <h1>Переиспользование</h1>
   <button class="btn primary">Показать сообщение</button>
-  <button class="btn">Перейти на главную</button>
+  <button class="btn" @click="navigate">Перейти на главную</button>
 </div>
 </template>
 
 <script>
+import {useRouter, useRoute} from 'vue-router'
 export default {
+  setup(){
+    const router = useRouter()
+    const route = useRoute()
+    const navigate = () => {
+      router.push('/')
+    }
+    return {
+      navigate,
+    }
+  }
 }
 </script>
 
