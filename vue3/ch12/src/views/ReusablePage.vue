a7eda2804fe5b635ff4093c2308a40485e8e804f
alami <alami@km.ru>
2021-09-03 18:36:39 +0500

    ch12-15 newAPI vs mixin.1

--- vue3/ch12/src/views/ReusablePage.vue
+++ vue3/ch12/src/views/ReusablePage.vue
@@ -1,24 +1,34 @@
 <template>
+<app-alert
+    v-if="alert"
+    title="Работаем с Composition"
+    type="primary"
+    @click="close"
+></app-alert>
 <div class="card">
   <h1>Переиспользование</h1>
-  <button class="btn primary">Показать сообщение</button>
+  <button class="btn primary" @click="toggle">{{ alert?'Закрыть':'Показать' }} сообщение</button>
   <button class="btn" @click="navigate">Перейти на главную</button>
 </div>
 </template>
 
 <script>
+import {ref} from 'vue'
 import {useRouter, useRoute} from 'vue-router'
+import AppAlert from '../AppAlert'
 export default {
   setup(){
+    const alert = ref(false)
     const router = useRouter()
     const route = useRoute()
-    const navigate = () => {
-      router.push('/')
-    }
+    const navigate = () => router.push('/')
+    const toggle = () => alert.value = !alert.value
+    const close = () => alert.value = false
     return {
-      navigate,
+      navigate, alert, toggle, close,
     }
-  }
+  },
+  components: {AppAlert},
 }
 </script>
 
