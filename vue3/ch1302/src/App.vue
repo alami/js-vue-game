60c05f11710a9cfb40a60b9ac6a4fcbc0c857ade
alami <alami@km.ru>
2021-09-04 12:50:26 +0500

    ch13-02 HelloWorld init

---
+++ vue3/ch1302/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="card center">
+      <img src="./assets/vue_logo.png" alt="Vue logo" />
+      <HelloWorld msg="Welcome to Your Vue.js App"></HelloWorld>
+    </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+export default {
+  components: {HelloWorld},
+}
+</script>
+
