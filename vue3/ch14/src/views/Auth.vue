cd72a0a7248f144369b7960b06e643b46da3e09f
alami <alami@km.ru>
2021-09-06 12:52:25 +0500

    ch14-03 Auth validation

--- vue3/ch14/src/views/Auth.vue
+++ vue3/ch14/src/views/Auth.vue
@@ -1,20 +1,33 @@
 <template>
-<form class="card" @submit.prevent>
+<form class="card" @submit.prevent="onSubmit">
   <h1>Войти в систему</h1>
-  <div class="form-control">
+
+  <div :class="['form-control',{invalid: eError}]">
     <label for="email">Email</label>
-    <input type="email" id="email" />
+    <input type="email" id="email" v-model="email" @blur="eBlur"/>
+    <small v-if="eError">{{eError}}</small>
   </div>
-  <div class="form-control">
-    <label for="password">Email</label>
-    <input type="password" id="password" />
+
+  <div :class="['form-control',{invalid: pError}]">
+    <label for="password">Password</label>
+    <input type="password" id="password" v-model="password"  @blur="pBlur"/>
+    <small v-if="pError">{{pError}}</small>
   </div>
-  <button class="btn primary" type="submit">Войти</button>
+
+  <button class="btn primary" type="submit"
+          :disabled="isSubmitting || isTooManyAttemps">Войти</button>
+  <div class="text-danger" v-if="isTooManyAttemps">
+    Вы слишком часто пытаетесь войти в систему. Попробуйте позже.
+  </div>
 </form>
 </template>
 
 <script>
+import {useLoginForm} from '@/use/login-form'
 export default {
+  setup() {
+    return {...useLoginForm()}
+  }
 }
 </script>
 
