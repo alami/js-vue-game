1dc8edddc3230c8592308bfbc1cd3234ac2b672e
alami <alami@km.ru>
2021-09-11 17:00:05 +0500

    ch14-06 error view v1

---
+++ vue3/ch14/src/components/ui/AppMessage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div :class="['alert', 'danger']">
+    <pre>{{message}}</pre>
+    <p class="alert-title">title</p>
+    <p>message, value</p>
+    <span class="alert-close">&times;</span>
+  </div>
+</template>
+
+<script>
+import {useStore} from "vuex";
+import {computed} from "vue";
+
+export default {
+  setup () {``
+    const store = useStore()
+
+    const message = computed(() => store.state.message)
+    return {message}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
